17782facc75680862a8ec2bd0e990d461e58cbfb
jadhavmadhav <jadhavmadhav1998@outlook.com>
2022-11-30 13:15:26 +0530

    unique email validation

--- controller/postUser.js
+++ controller/postUser.js
@@ -3,6 +3,7 @@ const userModel = require("../model/User")
 
 const postUserController = async(req,res)=>{
 const userId = (await userModel.find()).length + 1
+try {
      const postData = await userModel({
           userId: userId,
           userName:req.body.userName,
@@ -10,7 +11,12 @@ const userId = (await userModel.find()).length + 1
           password:req.body.password
      })
 
-     postData.save(postData).then(res=>console.log(res))
+     postData.save(postData).then(resp=>res.json({statusCode:200,message:'registretion sucessfully . !'}))
+     .catch(err=>res.json({statusCode:404,message:'Email is Already registered.!'}))
+} catch (error) {
+ res.send(error)
+}
+   
 }
 
 module.exports = postUserController
--- model/User.js
+++ model/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
      userId: {
@@ -13,13 +13,14 @@ const userSchema = mongoose.Schema({
      email: {
           type: String,
           required: true,
+          unique:true
      },
      password: {
           type: String,
           required: true
      }
 })
-
+userSchema.plugin(uniqueValidator);
 const userModel = mongoose.model('user', userSchema)
 
 module.exports = userModel
